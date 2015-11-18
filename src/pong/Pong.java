d341c102091c9edcc3d45421726f52d107da4489
1DAW11 <1DAW11@192.168.201.57>
2015-11-18 13:02:52 +0100

    Marcadores válidos (18/11/2015)

--- src/pong/Pong.java
+++ src/pong/Pong.java
@@ -8,16 +8,14 @@ package pong;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Point2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
@@ -30,18 +28,20 @@ public class Pong extends Application {
     final int WORLD_WIDTH = 500;
     final int WORLD_HEIGHT = 400;
     
-    double direccionX = 2;
+    double direccionX = 0;
     
-    double direcBolaY = 2;
-    double direcJug1Y = 2;
-    double direcJug2Y = 2;
+    double direcBolaY = 0;
+    double direcJug1Y = 0;
+    double direcJug2Y = 0;
     final double TAM_PALAS = 30;
-    
-    double ladoAlto = 0;
+    final double RADIO_B = 2.5;
+           
+    double ladoAlto = 50;
     double ladoBajo = 400; 
     double ladoDcho = 500;
     double ladoIzq = 0;
-    
+    int marcador_1 = 0;
+    int marcador_2 = 0;
 
     public void start(Stage primaryStage) {
         
@@ -52,26 +52,40 @@ public class Pong extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
         
-        // Creación de bola y palas
+        // PELOTA
         Circle bola = new Circle(5);
-        bola.setTranslateX(100);
-        bola.setTranslateY(100);
+        bola.setTranslateX(WORLD_WIDTH / 2);
+        bola.setTranslateY(WORLD_HEIGHT / 2);
         bola.setFill(Color.WHITE);
         root.getChildren().add(bola);
-               
+        
+        // JUGADORES
         Rectangle jugador1 = new Rectangle (5, 30);
         jugador1.setTranslateX(10);
-        jugador1.setTranslateY(20);
+        jugador1.setTranslateY(180);
         jugador1.setFill(Color.WHITE);
         root.getChildren().add(jugador1);
         
         Rectangle jugador2 = new Rectangle (5, 30);
         jugador2.setTranslateX(490);
-        jugador2.setTranslateY(20);
+        jugador2.setTranslateY(180);
         jugador2.setFill(Color.WHITE);
         root.getChildren().add(jugador2);
     
-       
+//         MARCADORES
+        Label score1 = new Label(String.valueOf(marcador_1));
+        score1.setTranslateX(125);
+        score1.setTranslateY(20);
+        score1.setTextFill(Color.WHITE);
+        root.getChildren().add(score1);
+        
+        Label score2 = new Label(String.valueOf(marcador_2));
+        score2.setTranslateX(350);
+        score2.setTranslateY(20);
+        score2.setTextFill(Color.WHITE);
+        root.getChildren().add(score2);
+        
+        
     new AnimationTimer() {
             @Override
         public void handle(long now) {
@@ -93,13 +107,13 @@ public class Pong extends Application {
             jug2Y += direcJug2Y;
             jugador2.setTranslateY(jug2Y);
               
-            // BOLA
+            // Dirección de la bola               
             if(posBX == jugador2.getTranslateX()){
                 direccionX = -2;
                 if(posBY >= jug2Y && posBY <= jug2Y + TAM_PALAS){
-                   direcJug2Y = -2;                                      
+                    direcJug2Y = -2;                                      
                 }else{ 
-                     direccionX = 2;
+                    direccionX = 2;
                 }
             }
             if(posBX == jugador1.getTranslateX()){
@@ -107,8 +121,8 @@ public class Pong extends Application {
                 if(posBY >= jug1Y && posBY <= jug1Y + TAM_PALAS){
                    direcJug1Y = 2;                                      
                 }else{ 
-                     direccionX = -2;
-                }
+                   direccionX = -2;
+                }          
             }
           
             if (posBY == ladoBajo){
@@ -120,19 +134,28 @@ public class Pong extends Application {
               
             // JUGADORES       
             if(jug1Y == ladoBajo){
-                direcJug1Y = -2;
+                direcJug1Y = 0;
             }
             if (jug1Y == ladoAlto){
-                direcJug1Y = 2;
+                direcJug1Y = 0;
             }                 
             if(jug2Y == ladoBajo){
-                direcJug2Y = -2;
+                direcJug2Y = 0;
             }
             if (jug2Y == ladoAlto){
-                direcJug2Y = 2;
+                direcJug2Y = 0;
             }
-
             
+            // MARCADORES
+            if(posBX == ladoIzq){
+                marcador_2 ++;
+                score2.setText(Integer.toString(marcador_2));
+                
+            }
+            if(posBX == ladoDcho){
+                marcador_1 ++;
+                score1.setText(Integer.toString(marcador_1));    
+            }
         }
             
     }.start(); 
@@ -153,7 +176,15 @@ public class Pong extends Application {
                 case DOWN:
                     direcJug2Y = 2;
                     break;
-            }      
+                
+                case SPACE:
+                    direccionX = 2;
+                    direcBolaY = 2;
+                    direcJug1Y = 2;
+                    direcJug2Y = 2;
+                    break;
+            }   
+            
         }
     });
 
